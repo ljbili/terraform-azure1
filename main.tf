43a01999249aeb748afd0e11b3e31d626d049789
ljbili <lokmane@3e9b17ecafd5>
2022-03-10 15:00:45 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,81 @@
+resource "random_string" "uid" {
+    special = false
+    upper = false
+    length = 6
+}
+
+resource "azurerm_resource_group" "rg-demo" {
+  name     = "rg-${var.namespace}-demo"
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg-demo.name
+  location                 = azurerm_resource_group.rg-demo.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "lok-resources"
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "lok-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "lok-nic"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                            = "lok-vm"
+  resource_group_name             = azurerm_resource_group.main.name
+  location                        = azurerm_resource_group.main.location
+  size                            = "Standard_F2"
+  admin_username                  = "adminuser"
+  admin_password                  = "P@ssw0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+output "resource_group_name" {
+  value = azurerm_storage_account.example.resource_group_name
+}
